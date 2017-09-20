9b2cca145a874e0636fc8f0b88a3c2d1c225319f
tomaszpio@gmail.com <kemot1@3>
2017-09-20 09:59:16 +0200

    cache removed

--- src/main/java/com/tp/loadbalancer/controller/LoadBalancerController.java
+++ src/main/java/com/tp/loadbalancer/controller/LoadBalancerController.java
@@ -6,28 +6,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 public class LoadBalancerController {
 
     private final RequestsDispatcher requestsDispatcher;
-    private final Map<String,String> requestsCache = new ConcurrentHashMap<>();
-    public LoadBalancerController(final RequestsDispatcher requestsDispatcher) {
 
+    public LoadBalancerController(final RequestsDispatcher requestsDispatcher) {
         this.requestsDispatcher = requestsDispatcher;
     }
 
     @RequestMapping("/route")
     public String route(@RequestParam(value="id")final String id){
 
-//        if(requestsCache.containsKey(id)) {
-//            return requestsCache.get(id);
-//        }else {
-//            final String group = this.requestsDispatcher.dispatch(id);
-//            requestsCache.put(id, group);
-//            return group;
-//        }
         return this.requestsDispatcher.dispatch(id);
     }
 }
